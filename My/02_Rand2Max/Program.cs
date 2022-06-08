82c7da15c430b40063fb98912457f697e07c94a1
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-08 16:03:23 +0300

    add 3 multiple

--- My/02_Rand2Max/Program.cs
+++ My/02_Rand2Max/Program.cs
@@ -1,9 +1,9 @@
-﻿// Напишите программу, которая выводит случайное число из отезка [10,99]
+﻿// Напишите программу, которая выводит случайное число из отезка [10, 99]
 // и показывает наибольшую цифру числа
 using System;
 using static System.Console;
 
 Clear();
 int number = new Random().Next(10, 100);
-if ((number / 10) > (number%10))	Write("{0} -> {1}", number, number / 10);
+if ((number / 10) > (number % 10))	Write("{0} -> {1}", number, number / 10);
 else								Write("{0} -> {1}", number, number % 10);
---
+++ My/03_Multiple/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить
+// является ли второе число кратное превому. Если число 2 не кратно
+// числу 1, то программа выводит остаток от деления
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите 1-ое число: ");
+int number1 = Convert.ToInt32(ReadLine());
+Write("Введите 2-ое число: ");
+int number2 = Convert.ToInt32(ReadLine());
+
+if ((number1 % number2) == 0)	Write("{0}, {1} -> кратно", number1, number2);
+else							Write("{0}, {1} -> не кратно, остаток {2}", number1, number2, number1 % number2);
