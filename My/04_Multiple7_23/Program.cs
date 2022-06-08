285330b41309822a650441a1e8cfd403ec3a8bdc
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-08 19:40:18 +0300

    add 4 multiple 7 and 23

---
+++ My/04_Multiple7_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+
+if (((number % 7) == 0) && ((number % 23) == 0))
+	Write("{0} -> да", number);
+else
+	Write("{0} -> нет", number);
