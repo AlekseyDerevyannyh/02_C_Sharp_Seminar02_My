ddc736afbd249f63497426ae4ef695f3507e1b74
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-08 20:27:32 +0300

    add 2 delete second number

---
+++ HW/02_DelN2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное
+// число и удаляет вторую цифру этого числа
+using System;
+using static System.Console;
+
+Clear();
+int number = new Random().Next(100, 1000);
+Write("{0} -> {1}", number, (number / 100) * 10 + (number % 10));
