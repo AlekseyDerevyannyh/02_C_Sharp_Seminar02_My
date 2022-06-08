a018070fed9241c14c3b0d3c2daf06e43b81b8a4
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-08 15:31:12 +0300

    add 2 max number of random

--- 02_Rand2Max/Program.cs
+++ 02_Rand2Max/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит случайное число из отезка [10,99]
+// и показывает наибольшую цифру числа
+using System;
+using static System.Console;
+
+Clear();
+int number = new Random().Next(10, 100);
+if ((number / 10) > (number%10))	Write("{0} -> {1}", number, number / 10);
+else								Write("{0} -> {1}", number, number % 10);
