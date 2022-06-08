4a4490da6abf8050e765933a8ea7ce8f06c091aa
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-08 20:05:06 +0300

    add HW 1 Second N of number

---
+++ HW/01_SecondN_OfNumber/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное
+// число и на выходе показывает вторую цифру этого числа
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(ReadLine());
+
+if((Math.Abs(number) < 100) || (Math.Abs(number) > 999)) {
+	Write("ОШИБКА! Число не трёхзначное!!!");
+	return;
+}
+Write("{0} -> {1}", number, (number / 10) % 10);
